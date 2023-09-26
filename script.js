bbf8ec6d188d68e21e77113becfa9e78a7e7f5a5
balavignesh111 <balavignesh1018@gmail.com>
2023-09-27 01:06:52 +0530

    project completed

--- script.js
+++ script.js
@@ -3,6 +3,17 @@
 
 const sliderImageContainer = document.querySelector('.slider-img');
 
+const nameEle = document.querySelector('#inp-name');
+
+const emailEle = document.getElementById("inp-email");
+
+const subEle = document.getElementById("inp-subject");
+
+const messageEle = document.getElementById("inp-message");
+
+const resetBtn = document.querySelector('.reset');
+
+
 // gv
 const data = ["image_00.jpg","image_01.jpg","image_02.jpg","image_03.jpg"];
 let index = 0;
@@ -19,3 +30,11 @@ setInterval(()=>{
   sliderImageContainer.src = imgLocation + data[index];
   
 },2000)
+
+
+resetBtn.addEventListener('click',()=>{
+  nameEle.value = "";
+  emailEle.value = "";
+  subEle.value = "";
+  messageEle.value = "";
+})
